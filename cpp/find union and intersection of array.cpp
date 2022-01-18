88e95807224acfaf0f7747d1b97ffb765ac147cc
Pratik Suryawanshi <88839654+Pratik29121@users.noreply.github.com>
2022-01-18 22:05:27 +0530

    Update find union and intersection of array.cpp

--- cpp/find union and intersection of array.cpp
+++ cpp/find union and intersection of array.cpp
@@ -35,3 +35,56 @@ int main()
  
 }
 
+
+
+
+INTERSECTION OF ARRAYS 
+
+input : 9 3
+        1 3 2 3 3 4 5 5 6
+        3 3 5
+  
+output : 3  
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin >> n>> m;
+    int cnt=0;
+    int arr1[n];
+    int arr2[m];
+    for(int i=0;i<n;i++)
+    {
+        cin >> arr1[i];
+    }
+    for(int j=0;j<m;j++)
+    {
+        cin >> arr2[j];
+    }
+    sort(arr1,arr1+n);
+    sort(arr2,arr2+m);
+    
+    for(int i=0, j=0; i<n && j<m;)
+    {
+        if(arr1[i] == arr2[j])
+        {
+            i++;
+            j++;
+            cnt++;
+        }
+        else if(arr1[i] < arr2[j])
+        {
+            i++;
+        }
+        else
+         j++;
+        
+    }
+    cout << cnt;
+    
+}
